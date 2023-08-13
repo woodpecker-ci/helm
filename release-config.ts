80a8fe847c2345ebf4f4b52c8a1431a5f7a3ca97
Anbraten <anton@ju60.de>
2023-08-13 14:51:04 +0200

    add release config

---
+++ release-config.ts
@@ -0,0 +1,15 @@
+export default {
+  beforePrepare: async ({ exec, nextVersion }) => {
+    await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" Chart.yaml`);
+    await exec(
+      `sed -i '' 's/^version:/version: ${nextVersion}/' charts/agent/Chart.yaml`
+    );
+    await exec(
+      `sed -i '' 's/^version:/version: ${nextVersion}/' charts/server/Chart.yaml`
+    );
+    await exec("helm dependency update");
+    await exec(
+      "git add Chart.yaml charts/agent/Chart.yaml charts/server/Chart.yaml"
+    );
+  },
+};
