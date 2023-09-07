8f05f6d8bf5a061a191657669836c083cc7bc28d
pat-s <patrick.schratz@gmail.com>
2023-09-07 15:59:33 +0200

    update appVersion to latest release

--- release-config.ts
+++ release-config.ts
@@ -1,12 +1,15 @@
 export default {
   beforePrepare: async ({ exec, nextVersion }) => {
-    await exec(`apk add --no-cache helm`);
+    await exec(`apk add --no-cache helm curl`);
     await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" Chart.yaml`);
     await exec(
       `sed -i '' 's/^version:/version: ${nextVersion}/' charts/agent/Chart.yaml`
     );
     await exec(
       `sed -i '' 's/^version:/version: ${nextVersion}/' charts/server/Chart.yaml`
+    );
+    await exec(
+      `export UPSTREAM_VERSION=$(curl -s https://api.github.com/repos/woodpecker-ci/woodpecker/releases/latest | grep tag_name | grep -Eo "([0-9]+(\.[0-9]+)+)") && sed -i "s/^appVersion:.*$/appVersion: $UPSTREAM_VERSION/g" Chart.yaml charts/agent/Chart.yaml charts/server/Chart.yaml`
     );
     await exec("helm dependency update");
     await exec(
