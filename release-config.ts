fff37ef695cf618537e90b0beb2e0fe46d21c36d
pat-s <patrick.schratz@gmail.com>
2023-11-13 23:57:11 +0100

    prepend 'v' to appVersion

--- release-config.ts
+++ release-config.ts
@@ -9,7 +9,7 @@ export default {
       `sed -i '' 's/^version:/version: ${nextVersion}/' charts/server/Chart.yaml`
     );
     await exec(
-      `export UPSTREAM_VERSION=$(curl -s https://api.github.com/repos/woodpecker-ci/woodpecker/releases/latest | grep tag_name | grep -Eo "([0-9]+(\.[0-9]+)+)") && sed -i "s/^appVersion:.*$/appVersion: $UPSTREAM_VERSION/g" Chart.yaml charts/agent/Chart.yaml charts/server/Chart.yaml`
+      `export UPSTREAM_VERSION=$(curl -s https://api.github.com/repos/woodpecker-ci/woodpecker/releases/latest | grep tag_name | grep -Eo "([0-9]+(\.[0-9]+)+)") && sed -i "s/^appVersion:.*$/appVersion: v$UPSTREAM_VERSION/g" Chart.yaml charts/agent/Chart.yaml charts/server/Chart.yaml`
     );
     await exec("helm dependency update");
     await exec(
