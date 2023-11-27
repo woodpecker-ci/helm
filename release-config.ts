24977b68677aa6b6dda904c346e6efd70b21e3b5
Anbraten <anton@ju60.de>
2023-11-27 09:58:51 +0100

    Update release-config.ts

--- release-config.ts
+++ release-config.ts
@@ -1,7 +1,6 @@
 export default {
   beforePrepare: async ({ exec, nextVersion }) => {
     await exec(`apk add --no-cache helm curl`);
-    // This updates the 'version' in Chart.yaml, inferred from the version which is supposed to be released
     await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" charts/woodpecker/Chart.yaml`);
     await exec('helm dependency update');
     await exec(
