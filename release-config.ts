73df4358a87cd1f2fff9f6f6b2fb1c87726bac8e
pat-s <patrick.schratz@gmail.com>
2024-06-13 22:57:19 +0200

    order

--- release-config.ts
+++ release-config.ts
@@ -1,8 +1,8 @@
 export default {
   commentOnReleasedPullRequests: false,
   beforePrepare: async ({ exec, nextVersion }) => {
+    await exec(`apt-get install -y git curl`);
     await exec(`curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash`);
-    await exec(`apt-get install -y git`);
     await exec(`sed -i "s/^version:.*$/version: ${nextVersion}/g" charts/woodpecker/Chart.yaml`);
     await exec('helm dependency update');
     await exec(
